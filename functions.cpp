80b3d9320636ee6d0a3c970046a09bff05dad63f
bsee25034 <bsee25034@itu.edu.pk>
2026-03-29 20:51:18 +0500

    Commit -Lab 6

--- functions.cpp
+++ functions.cpp
@@ -3,10 +3,18 @@
 
 int sumOfSquares(int n) 
 {
+	if (n == 1)
+		return 1;
+	else
+	return (n * n) + sumOfSquares(n - 1);
 }
 
 int findFactorial(int n)
 {
+	if (n == 0 || n == 1)
+		return 1;
+	else
+		return n * findFactorial(n - 1);
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include "functions.h"
 
 
-int main() {
-   
+int main()
+{
+	int choice;
+	cout << "Enter Your choice : " << endl;
+	cin >> choice;
+
+	if (choice == 1)
+	{
+
+		int n;
+		cout << "Enter a number : ";
+		cin >> n;
+		cout << "Sum of square" << endl;
+	}
+	else if (choice == 2)
+	{
+		int n;
+		cout << "Enter a number : ";
+		cin >> n;
+		cout << "Factorial of number is : " << findFactorial(n) << endl;
+	}
+
+
+
+	return 0;
 }
